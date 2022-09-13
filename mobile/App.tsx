b452a2dbe8bdeb223c9acd3f48ce13be11b5e61f
Julius Caezar <julius.caezar25@gmail.com>
2022-09-13 12:10:19 -0400

    add component

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,38 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
-import {  StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Tio lucas no react native</Text>
-
-      <Button title='Aperte Aqui'/>
-
-      <StatusBar style="auto" backgroundColor='purple'/>
+    <View>
+      
     </View>
   );
 }
-
-interface ButtonProps {
-  title: string;
-}
-
-function Button(props: ButtonProps) {
-  return (  
-    <TouchableOpacity>
-      <Text>
-        {props.title}
-      </Text>
-    </TouchableOpacity>
-  )
-
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
