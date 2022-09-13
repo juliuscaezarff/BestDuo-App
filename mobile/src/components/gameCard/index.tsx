f6affc8e3d34889293f78b318a55fc8075be81a6
Julius Caezar <julius.caezar25@gmail.com>
2022-09-13 17:34:00 -0400

    add styles

--- mobile/src/components/gameCard/index.tsx
+++ mobile/src/components/gameCard/index.tsx
@@ -1,6 +1,9 @@
 import { TouchableOpacity, TouchableOpacityProps, ImageBackground, ImageSourcePropType } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+
 
 import { styles } from './styles';
+import { THEME } from '../../theme';
 
 export interface GameCardProps {
   id: string;
@@ -20,6 +23,13 @@ export function GameCard({ data, ...rest }: Props) {
         style={styles.cover}
         source={data.cover}
       />
+
+      <LinearGradient 
+        colors={THEME.COLORS.FOOTER}
+        style={styles.footer}
+      >
+
+      </LinearGradient>
 
     </TouchableOpacity>
   );
