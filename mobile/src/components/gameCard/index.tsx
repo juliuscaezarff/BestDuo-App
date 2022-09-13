d89500a557006430ebf2d7f80c76e02fd4c3466c
Julius Caezar <julius.caezar25@gmail.com>
2022-09-13 17:40:02 -0400

    add props

--- mobile/src/components/gameCard/index.tsx
+++ mobile/src/components/gameCard/index.tsx
@@ -1,36 +1,36 @@
-import { TouchableOpacity, TouchableOpacityProps, ImageBackground, ImageSourcePropType } from 'react-native';
-import { LinearGradient } from 'expo-linear-gradient';
+import {
+  TouchableOpacity,
+  TouchableOpacityProps,
+  ImageBackground,
+  ImageSourcePropType,
+  Text
+} from 'react-native'
+import { LinearGradient } from 'expo-linear-gradient'
 
-
-import { styles } from './styles';
-import { THEME } from '../../theme';
+import { styles } from './styles'
+import { THEME } from '../../theme'
 
 export interface GameCardProps {
-  id: string;
-  name: string;
-  ads: string;
-  cover: ImageSourcePropType;
+  id: string
+  name: string
+  ads: string
+  cover: ImageSourcePropType
 }
 
 interface Props extends TouchableOpacityProps {
-  data: GameCardProps;
+  data: GameCardProps
 }
 
 export function GameCard({ data, ...rest }: Props) {
   return (
     <TouchableOpacity style={styles.container} {...rest}>
-      <ImageBackground 
-        style={styles.cover}
-        source={data.cover}
-      />
+      <ImageBackground style={styles.cover} source={data.cover}>
+        <LinearGradient colors={THEME.COLORS.FOOTER} style={styles.footer}>
+          <Text style={styles.name}>{data.name}</Text>
 
-      <LinearGradient 
-        colors={THEME.COLORS.FOOTER}
-        style={styles.footer}
-      >
-
-      </LinearGradient>
-
+          <Text style={styles.ads}>{data.ads} anúncios</Text>
+        </LinearGradient>
+      </ImageBackground>
     </TouchableOpacity>
-  );
+  )
 }
