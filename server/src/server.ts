d4b66e452b51ae1ae4bc0eda8ac5919e9f4e5f2a
Julius Caezar <julius.caezar25@gmail.com>
2022-09-13 22:15:14 -0400

    cret

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,7 +2,20 @@ import express from 'express'
 
 const app = express()
 
-app.get('/ads', (request, response) => {
+app.get('/games', (request, response) => {
+  return response.json([])
+})
+
+app.post('/ads', (_request, response) => {
+  return response.status(201).json([])
+})
+app.post('/ads', (_request, response) => {
+  return response.status(201).json([])
+})
+
+app.get('/games/:id/ads', (request, response) => {
+ // const gameId = request.params.id
+
   return response.json([
     { id: 1, name: ' Anúncio 1' },
     { id: 2, name: ' Anúncio 2' },
@@ -11,5 +24,11 @@ app.get('/ads', (request, response) => {
     { id: 5, name: ' Anúncio 5' },
   ])
 })
+
+app.get('/ads/:id/discord', (request, response) => {
+  // const adId = request.params.id
+ 
+   return response.json([])
+ })
 
 app.listen(3333)
