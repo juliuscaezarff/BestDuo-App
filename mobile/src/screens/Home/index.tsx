ab575aa3ec42f6f869f7cc92b98344b2cf94bbe5
Julius Caezar <julius.caezar25@gmail.com>
2022-09-13 22:14:45 -0400

    new files

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -32,6 +32,7 @@ export function Home() {
         )}
         showsHorizontalScrollIndicator={false}
         horizontal
+        contentContainerStyle={styles.contentList}
       />
 
     </View>
