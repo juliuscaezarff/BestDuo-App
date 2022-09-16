80d59a27d16add1033e367fb72271995c8786a3c
Julius Caezar <julius.caezar25@gmail.com>
2022-09-16 18:33:05 -0400

    connection to api and render list

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,20 +1,21 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { View, Image, FlatList } from 'react-native'
 
 import logoImg from '../../assets/logo-nlw-esports.png'
 
 import { Heading } from '../../components/Heading'
-import { GameCard } from '../../components/GameCard'
-
-import { GAMES } from '../../utils/games'
+import { GameCard, GameCardProps } from '../../components/GameCard'
 
 import { styles } from './styles'
 
 export function Home() {
+
+  const [games, setGames] = useState<GameCardProps[]>([])
+
   useEffect(() => {
     fetch('http://10.0.0.206:3333/games')
       .then(response => response.json())
-      .then(data => console.log(data))
+      .then(data => setGames(data))
   }, [])
 
   return (
@@ -27,7 +28,7 @@ export function Home() {
       />
 
       <FlatList
-        data={GAMES}
+        data={games}
         keyExtractor={item => item.id}
         renderItem={({ item }) => <GameCard data={item} />}
         showsHorizontalScrollIndicator={false}
