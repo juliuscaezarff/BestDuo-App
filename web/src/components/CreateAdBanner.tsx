286db04e5b63ef2afd45edd7e731af8ef469a31c
Julius Caezar <julius.caezar25@gmail.com>
2022-09-16 11:01:00 -0400

    create dialog trigger

--- web/src/components/CreateAdBanner.tsx
+++ web/src/components/CreateAdBanner.tsx
@@ -13,10 +13,10 @@ export function CreateAdBanner() {
             Publique um anúncio para encontrar novos players!
           </span>
         </div>
-        <button className="py-3 px-4 bg-violet-500 hover:bg-violet-700 text-white rounded flex items-center gap-3">
+        <Dialog.Trigger className="py-3 px-4 bg-violet-500 hover:bg-violet-700 text-white rounded flex items-center gap-3">
           <MagnifyingGlassPlus size={24} />
           Publicar anúncio
-        </button>
+        </Dialog.Trigger>
       </div>
     </div>
   )
