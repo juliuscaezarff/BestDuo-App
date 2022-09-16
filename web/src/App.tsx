f44c7b886bc882104a9035ef2cd5c6e8b97063d1
Julius Caezar <julius.caezar25@gmail.com>
2022-09-16 00:11:16 -0400

    create gamebanner and conection to api

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -6,9 +6,17 @@ import './styles/main.css'
 import logo from './assets/logo-nlw-esports.svg'
 import { CreateAdBanner } from './components/CreateAdBanner'
 
-function App() {
+interface Game {
+  id: string
+  title: string
+  bannerUrl: string
+  _count: {
+    ads: number
+  }
+}
 
-  const [games, setGames] = useState([])
+function App() {
+  const [games, setGames] = useState<Game[]>([])
 
   useEffect(() => {
     fetch('http://localhost:3333/games')
@@ -16,7 +24,6 @@ function App() {
       .then(data => {
         setGames(data)
       })
-
   }, [])
 
   return (
@@ -32,8 +39,16 @@ function App() {
       </h1>
 
       <div className="grid grid-cols-6 gap-6 mt-16">
-        <GameBanner bannerUrl='/game-1.png' title="league of legends" adsCount={5} />
-        <GameBanner bannerUrl='/game-2.png' title="Dota 2" adsCount={5} />
+        {games.map(game => {
+          return (
+            <GameBanner
+              key={game.id}
+              bannerUrl={game.bannerUrl}
+              title={game.title}
+              adsCount={game._count.ads}
+            />
+          )
+        })}
       </div>
 
       <CreateAdBanner />
