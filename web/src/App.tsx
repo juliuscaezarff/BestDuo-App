1b14b7c97ddd96b6136a421b47c13a17341aae72
Julius Caezar <julius.caezar25@gmail.com>
2022-09-15 22:20:18 -0400

    add component in app

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,4 +1,5 @@
 import { MagnifyingGlassPlus } from 'phosphor-react'
+import { GameBanner } from './components/GameBanner'
 
 import './styles/main.css'
 
@@ -18,16 +19,7 @@ function App() {
       </h1>
 
       <div className="grid grid-cols-6 gap-6 mt-16">
-        <a href="" className="relative rounded-lg overflow-hidden">
-          <img src="/game-1.png" alt="" />
-
-          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 right-0 left-0">
-            <strong className="font-bold text-white block">
-              League of Legends
-            </strong>
-            <span className="text-zinc-300 text-sm block">4 anúncios</span>
-          </div>
-        </a>
+        <GameBanner />
         <a href="" className="relative rounded-lg overflow-hidden">
           <img src="/game-2.png" alt="" />
           <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 right-0 left-0">
