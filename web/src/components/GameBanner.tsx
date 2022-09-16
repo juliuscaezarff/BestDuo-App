a3babaa76eda9f186d3376eb28c267b9c5147a67
Julius Caezar <julius.caezar25@gmail.com>
2022-09-15 22:25:06 -0400

    create props

--- web/src/components/GameBanner.tsx
+++ web/src/components/GameBanner.tsx
@@ -1,14 +1,21 @@
-export function GameBanner() {
+interface GameBannerProps {
+  bannerUrl: string
+  title: string
+  adsCount: number
+}
+
+
+export function GameBanner(props: GameBannerProps) {
 
   return (
     <a href="" className="relative rounded-lg overflow-hidden">
-          <img src="/game-1.png" alt="" />
+          <img src={props.bannerUrl} alt="" />
 
           <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 right-0 left-0">
             <strong className="font-bold text-white block">
-              League of Legends
+              {props.title}
             </strong>
-            <span className="text-zinc-300 text-sm block">4 anúncios</span>
+            <span className="text-zinc-300 text-sm block">{props.adsCount} anúncio(s)</span>
           </div>
         </a>
   )
