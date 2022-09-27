42164a4243abc769afb7d567c7ef388bf7a35dca
Julius Caezar <julius.caezar25@gmail.com>
2022-09-27 12:54:44 -0400

    add style to signIn

--- mobile/src/screens/Signin/styles.ts
+++ mobile/src/screens/Signin/styles.ts
@@ -1,5 +1,30 @@
 import { StyleSheet } from 'react-native';
+import { THEME } from '../../theme';
 
 export const styles = StyleSheet.create({
-  container: {}
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  logo: {
+    width: 214,
+    height: 120,
+    marginTop: 74,
+    marginBottom: 48,
+  },
+  button: {
+    width: "100%",
+    height: 36,
+    borderRadius: 6,
+    backgroundColor: THEME.COLORS.PRIMARY,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonTitle: {
+    color: THEME.COLORS.TEXT,
+    fontFamily: THEME.FONT_FAMILY.SEMI_BOLD,
+    fontSize: THEME.FONT_SIZE.SM,
+    marginLeft: 8,
+  }
 });
