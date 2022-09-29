19e957bbe08933f2239fedd381b8426a1f727487
Julius Caezar <julius.caezar25@gmail.com>
2022-09-28 22:31:19 -0400

    create interface SliderProps

--- web/src/components/Slider/Slider.tsx
+++ web/src/components/Slider/Slider.tsx
@@ -1,11 +1,17 @@
-import { Swiper, SwiperSlide } from 'swiper/react'
+import { Swiper, SwiperSlide, SwiperProps } from 'swiper/react'
+import { ReactNode } from 'react'
 
 import 'swiper/css'
 
-export default function Slider() {
+interface SliderProps {
+  settings: SwiperProps;
+  children: ReactNode;
+}
+
+export default function Slider({ settings, children }: SliderProps ) {
   return (
-    <Swiper>
-      
+    <Swiper {...settings}>
+      {children}
     </Swiper>
   )
 }
