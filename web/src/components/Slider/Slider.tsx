ae8d9fabf4e1bcdbe507b86fecf06c835dedf83a
Julius Caezar <julius.caezar25@gmail.com>
2022-09-28 22:18:02 -0400

    create new component Slider

---
+++ web/src/components/Slider/Slider.tsx
@@ -0,0 +1,21 @@
+export default function Slider() {
+  return (
+    <Swiper>
+      <SwiperSlide>
+        <h1>Teste</h1>
+      </SwiperSlide>
+      <SwiperSlide>
+        <h1>Teste</h1>
+      </SwiperSlide>
+      <SwiperSlide>
+        <h1>Teste</h1>
+      </SwiperSlide>
+      <SwiperSlide>
+        <h1>Teste</h1>
+      </SwiperSlide>
+      <SwiperSlide>
+        <h1>Teste</h1>
+      </SwiperSlide>
+    </Swiper>
+  )
+}
