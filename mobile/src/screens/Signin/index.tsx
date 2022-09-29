8fa11c84b4e8b207f1e24b44c6a5335167a310aa
Julius Caezar <julius.caezar25@gmail.com>
2022-09-28 20:30:06 -0400

    create fetch to api discord

--- mobile/src/screens/Signin/index.tsx
+++ mobile/src/screens/Signin/index.tsx
@@ -1,19 +1,33 @@
 import { useEffect, useState } from 'react'
+import * as AuthSession from 'expo-auth-session';
 import { Image, TouchableOpacity, Text   } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { GameController } from 'phosphor-react-native'
 
+import { styles } from './styles'
+import { THEME } from '../../theme'
 import logoImg from '../../assets/logo-nlw-esports.png'
 
 import { Heading } from '../../components/Heading'
 import { Background } from '../../components/Background'
 
-import { styles } from './styles'
 
 export function SignIn() {
 
   async function handleDiscordSignIn() {
 
+   const response = await AuthSession.startAsync({
+      authUrl: "https://discord.com/api/oauth2/authorize?client_id=1024345562872565811&redirect_uri=https%3A%2F%2Fauth.expo.io%2F%40julius_caezar%2Fmobile&response_type=token&scope=identify"
+    })
+
+    fetch('https://discord.com/api/users/@me', {
+      headers: {
+        'authorization': `Bearer ${response.params.access_token}`
+      }
+    })
+    .then(response => response.json())
+    .then()
+    
   }
 
   return (
@@ -32,7 +46,7 @@ export function SignIn() {
       >
       
       <GameController
-        color={}
+        color={THEME.COLORS.TEXT}
         size={20}
       />
 
