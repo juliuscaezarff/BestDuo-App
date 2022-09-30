19439a2c098dd193d9afdf9b07f9cfc213fdea61
Julius Caezar <julius.caezar25@gmail.com>
2022-09-29 23:35:57 -0400

    import slider.css

--- web/src/components/Slider/Slider.tsx
+++ web/src/components/Slider/Slider.tsx
@@ -2,6 +2,7 @@ import { Swiper, SwiperSlide, SwiperProps } from 'swiper/react'
 import { ReactNode } from 'react'
 import { Navigation, Pagination, A11y } from 'swiper';
 
+import './Slider.css'
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
